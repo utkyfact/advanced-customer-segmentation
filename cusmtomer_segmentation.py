36544da2e3c0a730b7a51dbcb9107bdfa33d1c53
Utku Şen <utkusen9@gmail.com>
2024-10-03 14:50:41 +0300

    simplification

--- cusmtomer_segmentation.py
+++ cusmtomer_segmentation.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # İLERİ SEVİYE MÜŞTERİ SEGMENTASYONU PROJESİ
-# 
-# Bu projemizde Massachusetts Institute of Technology (MIT) tarafından geliştirilmiş ileri seviye bir kütüphane kullanacağız.
-# Verilerimiz komplex olduğu için burada K-Means kullanamıyoruz.
 
 # In[1]:
 
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 
 
 # In[2]:
@@ -18,10 +12,7 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib import pyplot as plt
-# kmodes kütüphanesi standart distribution içinde gelmez sizin install etmeniz gerekebilir.
-# Massachusetts Institute of Technology (MIT) tarafından geliştirilmiş ileri seviye bir kütüphanedir.
-# https://anaconda.org/conda-forge/kmodes linkinde detay mevcuttur
-# conda install -c conda-forge kmodes   komutu ile Anaconda üzerinde kurabilirsiniz..
+
 from kmodes.kprototypes import KPrototypes  
 from kmodes.kmodes import KModes
 
@@ -42,7 +33,7 @@ df.tail()
 # In[5]:
 
 
-# We have no null data see:
+
 df.isnull().sum()
 
 
@@ -51,7 +42,6 @@ df.isnull().sum()
 
 # ## Income ve Age Data Normalization
 
-# Before Scaling/Normalization we keep our normal values in temp variables..
 df_temp = df[['ID','Age', 'Income']]
 df_temp
 
@@ -72,7 +62,7 @@ df['Income'] = scaler.transform(df[['Income']])
 
 
 # Drop ID before analysis..
-# Since ID is not used in analysis...
+
 df = df.drop(['ID'], axis=1)
 
 
@@ -95,7 +85,7 @@ df.head()
 # In[11]:
 
 
-# Build our model...
+# Build model
 
 kproto = KPrototypes(n_clusters=10, verbose=2, max_iter=20)
 clusters = kproto.fit_predict(mark_array, categorical=[0, 1, 3, 5, 6])
@@ -114,9 +104,7 @@ for c in clusters:
     cluster_dict.append(c)
 
 
-
 df['cluster']=cluster_dict
-
 
 
 # Put original columns from temp to df:
@@ -160,9 +148,6 @@ plt.scatter(df8.Age, df8['Income'],color='gray', alpha = 0.4)
 plt.scatter(df9.Age, df9['Income'],color='purple', alpha = 0.4)
 plt.scatter(df10.Age, df10['Income'],color='blue', alpha = 0.4)
 
-
-# kmeans_modelim.cluster_centers_ numpy 2 boyutlu array olduğu için x ve y sütunlarını kmeans_modelim.cluster_centers_[:,0] 
-# ve kmeans_modelim.cluster_centers_[:,1] şeklinde scatter plot için alıyoruz:
 #plt.scatter(kmeans_modelim.cluster_centers_[:,0], kmeans_modelim.cluster_centers_[:,1], color='blue', marker='X', label='centroid')
 plt.legend()
 plt.show()
